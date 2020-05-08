3064ded4e5a6b3a17a240281b7e26964a2556213
charcharmasonjar <charlotte.dibb@gmail.com>
2020-05-07 18:27:52 -0600

    Create About section as a component

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <img src={require("./sitting.png")} className="about-image" />
+    </div>
+  );
+}
