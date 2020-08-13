16aa74b44c8982720d44cbb4fcd05247737376e5
charlottedibb <charlotte.dibb@gmail.com>
2020-08-13 14:29:06 -0600

    Display about section as three cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,31 @@
 import React from "react";
+import "../styles/About.css";
 
-export default function About() {
+export default function About(props) {
   return (
-    <div className="about">
-      <div className="about-text">
-        <p>
-          I’m a McGill University psychology grad with a past life in Human
-          Resources.
-        </p>
-        <p>
-          Seeking new and more interesting challenges, I took the leap into
-          programming and web development.
-        </p>
+    <div className="about-container" ref={props.refProp}>
+      <h2>A bit about me...</h2>
+      <div className="about-group">
+        <img src={require("../media/animat-responsive.gif")} alt="changing device size"/>
+        <h3>Design/Development</h3>
         <p>
           I love making beautiful, robust UIs for broad audiences, solving
           tricky problems, and seeing my applications come to life.
         </p>
       </div>
-      <div className="sitting">
-        <p>When I'm not coding, you can find me at home. </p>
-        <img src={require("./sitting.png")} className="about-image" alt="cartoon sitting person"/>
+      <div className="about-group">
+        <img src={require("../media/animat-lightbulb.gif")} alt="lightbulb"/>
+        <h3>Psychology</h3>
+        <p>
+          I'm a research assistant at the Bee Lab at Kwantlen Polytechnic
+          University, researching the connection between social media on mental
+          health.
+        </p>
+      </div>
+      <div className="about-group">
+        <img src={require("../media/animat-checkmark.gif")} alt="checkmark"/>
+        <h3>HR Background</h3>
+        <p>Jaw-droppingly professional. Bureaucracy fears me. </p>
       </div>
     </div>
   );
