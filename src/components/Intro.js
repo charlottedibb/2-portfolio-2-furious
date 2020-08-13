7934c229de10dcbc662cd25c838beab4ece1ac93
charlottedibb <charlotte.dibb@gmail.com>
2020-08-13 14:27:52 -0600

    Fade in text in intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import FadeIn from "react-fade-in";
+import { ReactComponent as Arrow } from "../media/arrow-down.svg";
 
-export default function Intro() {
+import "../styles/Intro.css";
+
+export default function Intro(props) {
+
+
   return (
     <div className="intro">
+      <img
+        src={require("../media/walking.png")}
+        className="intro-image"
+        alt="cartoon running person"
+      />
       <div className="intro-text">
-        <div>
-          <h2>Hey. I'm Charlotte. </h2>
-          <p>I'm a full stack web developer.</p>
-        </div>
+        <FadeIn delay={500}>
+          <h2>Hey.</h2>
+          <h2 className="text-wrap">I'm Charlotte.</h2>
+          <div onClick={props.executeScroll}>
+            <p>I'm a full stack web developer.</p>
+            <Arrow style={{ height: "48px", width: "48px" }} className="arrow"/>
+          </div>
+        </FadeIn>
       </div>
-      <img src={require("./walking.png")} className="intro-image" alt="cartoon running person"/>
     </div>
   );
 }
