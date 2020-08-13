9d8f04f427c7b607b4f0325c3b3fa5263fe9dec8
charlottedibb <charlotte.dibb@gmail.com>
2020-08-13 14:38:37 -0600

    Import components into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
-import Intro from "./components/Intro"
-import About from './components/About';
+import React, { useRef } from "react";
+import Intro from "./components/Intro";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import More from "./components/More";
+import MyForm from "./components/MyForm";
+import TechStack from "./components/TechStack";
+
+import "./App.css";
+import "./styles/Projects.css";
+import "./styles/Form.css";
+
+const scrollToRef = ref => window.scrollTo({ right: 0, top: ref.current.offsetTop, behavior: "smooth" });
 
 function App() {
+  const myRef = useRef(null);
+  const executeScroll = () => scrollToRef(myRef);
+
   return (
     <div className="App">
-      <Intro />
-      <About />
+      <Intro executeScroll={executeScroll} />
+      <About refProp={myRef} />
+      <TechStack />
+      <Projects />
+      <More />
+      <MyForm />
     </div>
   );
 }
