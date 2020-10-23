78fa8e87b33bdda2033142f2df85f6159d7bcc11
charlottedibb <charlotte.dibb@gmail.com>
2020-10-23 14:50:13 -0700

    Add react router and Resume component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React, { useRef } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Intro from "./components/Intro";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import More from "./components/More";
 import MyForm from "./components/MyForm";
 import TechStack from "./components/TechStack";
+import Resume from "./components/Resume"
 
 import "./App.css";
 import "./styles/Projects.css";
 import "./styles/Form.css";
 
-const scrollToRef = ref => window.scrollTo({ right: 0, top: ref.current.offsetTop, behavior: "smooth" });
+const scrollToRef = ref =>
+  window.scrollTo({ right: 0, top: ref.current.offsetTop, behavior: "smooth" });
 
 function App() {
   const myRef = useRef(null);
   const executeScroll = () => scrollToRef(myRef);
 
   return (
-    <div className="App">
-      <Intro executeScroll={executeScroll} />
-      <About refProp={myRef} />
-      <TechStack />
-      <Projects />
-      <More />
-      <MyForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <Intro executeScroll={executeScroll} />
+            <About refProp={myRef} />
+            <TechStack />
+            <Projects />
+            <More />
+            <MyForm />
+            <Link to="/resume">Resume</Link>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
